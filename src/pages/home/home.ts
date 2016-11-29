5de4dcfd30df0d516327a35eb78cdb79832199fa
Baruch Oltman <Baruch.Oltman@gmail.com>
2016-11-28 20:55:44 -0600

    Added Deposit and Withdraw button to home page. These buttons pass params to the new transaction page to specifiy which state the page should be in (what button was pressed). I also added a cancel button to the transaction page that uses .popToRoot().

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,27 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { NewTransactionPage } from '../new-transaction/new-transaction';
+
 @Component({
   selector: 'page-homepage',
   templateUrl: 'home.html'
+  
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+    transactionType = [{ type: 'Make A Withdrawal' }, { type: 'Make A Deposit' }];
+   // withdrawal: string = 'Make A Withdrawal';
+   // deposit: string = 'Make A Deposit';
+
+    constructor(public navCtrl: NavController) { }
+
+    pressedWithdraw() {
+        this.navCtrl.push(NewTransactionPage, this.transactionType[0]);
+    }
+
+    pressedDeposit() {
+        this.navCtrl.push(NewTransactionPage, this.transactionType[1]);     
+    }
 
 }
