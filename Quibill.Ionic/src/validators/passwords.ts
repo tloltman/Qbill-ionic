0e8d538ec85371726250a30cc3dd821b1b02d7b7
Baruch Oltman <baruch.oltman@outlook.com>
2017-03-26 09:03:51 -0500

    Finished implementing registration validation. Added validators folder to hold the custom password confirmation valdator class.

---
+++ Quibill.Ionic/src/validators/passwords.ts
@@ -0,0 +1,12 @@
+﻿import { FormControl } from '@angular/forms';
+
+export class ConfirmPasswordValidator {
+    static checkConfirmPassword(control: FormControl): any {
+
+        if (control.value == control.root.value['password']) {
+            return null;
+        } else {
+            return { isValid: true };
+        }
+    }
+}
